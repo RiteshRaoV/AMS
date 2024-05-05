98052cd52e4d88ec4ffb827e62726ce07f228ffe
Ritesh Rao V <ritheshraov016@gmail.com>
2024-05-05 13:50:06 +0530

    added logic to update the existing attendance record

--- src/main/java/com/thbs/attendance/Entity/Attendance.java
+++ src/main/java/com/thbs/attendance/Entity/Attendance.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.sql.Date;
 import java.util.List;
 
 import org.springframework.data.annotation.Id;
--- src/main/java/com/thbs/attendance/Service/AttendanceService.java
+++ src/main/java/com/thbs/attendance/Service/AttendanceService.java
@@ -13,7 +13,6 @@ import com.thbs.attendance.Repository.AttendanceRepository;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,30 +22,39 @@ public class AttendanceService {
     @Autowired
     private AttendanceRepository attendanceRepository;
 
-
-
     public Attendance processAttendanceUpdate(AttendanceUpdateDTO attendanceData) {
         Long batchId = attendanceData.getBatchId();
         Long courseId = attendanceData.getCourseId();
         String date = attendanceData.getDate();
         String type = attendanceData.getType();
         List<AttendanceDetailDTO> newAttendance = attendanceData.getAttendance();
-        List<Attendance> list=new ArrayList<>();
+
         for (AttendanceDetailDTO details : newAttendance) {
-            Attendance record = attendanceRepository.findByUserIdAndCourseIdAndBatchId(details.getUserId(), courseId, batchId);
-            list.add(record);
-            if(record!=null){
-                AttendanceDetail detail=new AttendanceDetail(date,type,details.getStatus());
-                record.getAttendance().add(detail);
+            Attendance record = attendanceRepository.findByUserIdAndCourseIdAndBatchId(details.getUserId(), courseId,
+                    batchId);
+            if (record != null) {
+                boolean updated = false;
+                List<AttendanceDetail> attendanceList = record.getAttendance();
+                for (AttendanceDetail obj : attendanceList) {
+                    if (obj.getDate().equals(date) && obj.getType().equals(type)) {
+                        obj.setStatus(details.getStatus());
+                        updated = true;
+                        break;
+                    }
+                }
+                if (!updated) {
+                    AttendanceDetail newDetail = new AttendanceDetail(date, type, details.getStatus());
+                    attendanceList.add(newDetail);
+                }
                 attendanceRepository.save(record);
-            }else{
-                AttendanceDetail detail = new AttendanceDetail(date,type,details.getStatus());
+            } else {
                 List<AttendanceDetail> attendanceList = new ArrayList<>();
-                attendanceList.add(detail);
-                Attendance attendance = new Attendance(batchId, courseId,details.getUserId(), attendanceList);
-                attendanceRepository.save(attendance);
-             }
+                attendanceList.add(new AttendanceDetail(date, type, details.getStatus()));
+                Attendance newAttendanceRecord = new Attendance(batchId, courseId, details.getUserId(), attendanceList);
+                attendanceRepository.save(newAttendanceRecord);
+            }
         }
         return null;
     }
+
 }
